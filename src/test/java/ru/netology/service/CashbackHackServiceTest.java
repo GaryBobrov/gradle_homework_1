403e90f7171da755986db902aeda307a0c0ef7b8
Bobrov Igor <bobrov.i.o@live.ru>
2023-04-25 21:46:18 +0300

    Add TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
-class CashbackHackServiceTest {
-  
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    public CashbackHackService Service = new CashbackHackService();
+
+    @Test
+    public void shouldCashBackSum999() {
+        int actual = Service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashBackSum1() {
+        int actual = Service.remain(1);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashBackSum1000() {
+        int actual = Service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashBackSum1200() {
+        int actual = Service.remain(1200);
+        int expected = 800;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashBackSum1800() {
+        int actual = Service.remain(1800);
+        int expected = 200;
+        assertEquals(actual, expected);
+    }
+
 }
