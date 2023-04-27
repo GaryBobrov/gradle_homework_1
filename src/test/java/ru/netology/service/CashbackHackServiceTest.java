362558af5dd8b3280acce563eb563f34b04dbf87
Bobrov Igor <bobrov.i.o@live.ru>
2023-04-27 19:58:15 +0300

    fixing homework

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,40 +6,40 @@ import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    public CashbackHackService Service = new CashbackHackService();
+    public CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldCashBackSum999() {
-        int actual = Service.remain(999);
+        int actual = service.remain(999);
         int expected = 1;
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldCashBackSum1() {
-        int actual = Service.remain(1);
+        int actual = service.remain(1);
         int expected = 999;
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldCashBackSum1000() {
-        int actual = Service.remain(1000);
+        int actual = service.remain(1000);
         int expected = 0;
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldCashBackSum1200() {
-        int actual = Service.remain(1200);
+        int actual = service.remain(1200);
         int expected = 800;
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldCashBackSum1800() {
-        int actual = Service.remain(1800);
-        int expected = 200;
+        int actual = service.remain(0);
+        int expected = 1000;
         assertEquals(actual, expected);
     }
 
